8425773568686b4bf8b03c171c15a151ee57c6e3
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-04-19 15:29:24 +0800

    +equationFirstVal , *ongoing equationSecondVal , +getOtherState

--- HiddenMarkov.java
+++ HiddenMarkov.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class HiddenMarkov {
     public ArrayList<String> values, measurement, compCases;
     public HashMap<String, Double> probabilities;
+    public ArrayList<Double> firstVal, secondVal;
     public int cases;
     public String sequence;
                     
@@ -18,29 +19,155 @@ public class HiddenMarkov {
         this.compCases = new ArrayList<String>();
         this.cases = 0;
     }
+
     public void run(){
         loadFile();
         getTransitionProbabilities();
+        getStates();
         saveFile();
     }
 
     public void getTransitionProbabilities(){
         char[] sq=sequence.toCharArray();
-        getS0(sq);
-        getTransitionProbability(sq,'S','S');
-        getTransitionProbability(sq,'T','S');
-        getTransitionProbability(sq,'T','T');
-        getTransitionProbability(sq,'S','T');
+        char first = values.get(0).charAt(0);
+        char second = values.get(1).charAt(0);
+        getFirstState(sq);
+        getTransitionProbability(sq,first,first);
+        getTransitionProbability(sq,second,first);
+        getTransitionProbability(sq,second,second);
+        getTransitionProbability(sq,first,second);
     }
 
-    public void getS0(char[] sq){
+    public void getFirstState(char[] sq){
+        String f = values.get(0);
+        f =  f.concat("0");
+        String s = values.get(1);
+        s =  s.concat("0");
         if(sq[0] == values.get(0).charAt(0)){
-            probabilities.put("S0", 1.0);            
+            probabilities.put(f, 1.0);            
+            probabilities.put(s, 0.0);            
         }
         else{
-            probabilities.put("S0", 0.0);            
+            probabilities.put(s, 1.0);            
+            probabilities.put(f, 0.0);            
         }
     }
+
+    public void getStates(){
+        // for(int index = 0; index < compCases.size(); index++){
+        //     getValueSubX(compCases.get(index));
+        // }
+        getValueSubX("S1 given E1");
+        getValueSubX("T1 given E1");
+    }
+
+
+
+    public void getValueSubX(String value){
+            String[] val = value.split("given"); //stores all the words from the line in values
+            String comp = val[0]; //S1
+            String measure = val[1]; //E1
+            String[] c = comp.split("");
+            String[] m = measure.split("");
+            String state = c[0];
+            int x = Integer.parseInt(c[1]);
+
+            if(state.equals(values.get(0))){ //S
+                System.out.println(comp + " " + state + " " + getOtherState(state) + " " + x);
+                equationFirstVal(comp, state, getOtherState(state), x);
+            }
+            else if(state.equals(values.get(1))){ //T
+                System.out.println(comp + " " + state + " " + getOtherState(state) + " " + x);
+                equationSecondVal(comp, state, getOtherState(state), x);
+            }
+            else if(state.equals(measurement.get(0))){ //E
+
+            }
+            else if(state.equals(measurement.get(1))){ //F
+
+            }
+            // x.toString(x);
+    }
+
+    public String getOtherState(String s){ //good
+        String ans = "";
+        for(int index = 0; index < values.size(); index++){
+            if(!(s.equals(values.get(index)))){
+                ans = values.get(index);
+            }
+        }
+        return ans;
+    }
+// P(S2) = P(SS)P(S1) + P(ST)P(T1)
+//siz good
+    public void equationFirstVal(String compStatement, String state,  String opState, int x){ //S1 , S, T,  1
+    // P(S1) = P(SS)P(S0) + P(ST)P(T0)
+        String Sminus = state.concat(Integer.toString(x-1)); //S0 
+        String Tminus = opState.concat(Integer.toString(x-1));  //T0
+        String SS = state.concat(state);  //SS
+        String ST = state.concat(opState);  //ST
+
+        if(probabilities.containsKey(Sminus) && probabilities.containsKey(Tminus)){ //if T0 & S0 exists fetch their probability
+            Double dS = probabilities.get(Sminus); 
+            Double dT = probabilities.get(Tminus);
+            Double dSS = probabilities.get(SS);
+            Double dST = probabilities.get(ST);
+            Double ans = dSS * dS + dST * dT;
+            System.out.println(Sminus + ": " + dS + " " + Tminus + ": " + dT + " " + SS + ": " + dSS + " " + ST + ": " + dST);
+            System.out.println(dSS + "*" + dS + "+" + dT + "*" + dST + "=" + ans);
+            probabilities.put(compStatement, ans);
+        }
+        else{
+            if(probabilities.containsKey(Sminus) && !(probabilities.containsKey(Tminus))){
+                equationSecondVal(Tminus, state, opState, x-1);
+            }
+            else if(!(probabilities.containsKey(Sminus)) && probabilities.containsKey(Tminus)){
+                equationFirstVal(Sminus, state, opState, x-1);
+            } 
+            else{
+                equationFirstVal(Sminus, state, opState, x-1);
+                equationSecondVal(Tminus, state, opState, x-1);
+            }
+        }
+    }
+
+// P(T1) = P(TS)P(S0) + P(TT)P(T0)
+// P(T2) = P(TS)P(S1) + P(TT)P(T1)
+    public void equationSecondVal(String compStatement, String state,  String opState, int x){
+        String Tminus = state.concat(Integer.toString(x-1)); //T0 
+        String Sminus = opState.concat(Integer.toString(x-1));  //S0
+        String TT = state.concat(state);  //TT
+        String TS = state.concat(opState);  //TS
+
+        if(probabilities.containsKey(Sminus) && probabilities.containsKey(Tminus)){ //if T0 & S0 exists fetch their probability
+            Double dT = probabilities.get(Tminus);
+            Double dS = probabilities.get(Sminus); 
+            Double dTS = probabilities.get(TS);
+            Double dTT = probabilities.get(TT);
+            Double ans = dTS * dS + dTT * dT;
+            System.out.println(Sminus + ": " + dS + " " + Tminus + ": " + dT + " " + TS + ": " + dTS + " " + TT + ": " + dTS);
+            System.out.println(dTS + "*" + dS + "+" + dT + "*" + dTT + "=" + ans);
+            probabilities.put(compStatement, ans);
+        }
+        else{
+            if(probabilities.containsKey(Sminus) && !(probabilities.containsKey(Tminus))){
+                equationSecondVal(Tminus, state, opState, x-1);
+            }
+            else if(!(probabilities.containsKey(Sminus)) && probabilities.containsKey(Tminus)){
+                equationFirstVal(Sminus, state, opState, x-1);
+            } 
+            else{
+                equationFirstVal(Sminus, state, opState, x-1);
+                equationSecondVal(Tminus, state, opState, x-1);
+            }
+        }
+    }
+
+    public void equationMeasure(){
+        
+    }
+
+//iterate over probability arraylist
     
     //x given y : followed by x (x ung after y)
     public void getTransitionProbability(char[] sq, char x, char y){
